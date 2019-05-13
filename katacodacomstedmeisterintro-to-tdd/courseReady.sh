38759042d74355df1953e9bcce54a33490458edc
Steddy Smit <jacob.smit@zepler.net>
2019-05-13 23:38:52 +0100

    Updated

--- katacodacomstedmeisterintro-to-tdd/courseReady.sh
+++ katacodacomstedmeisterintro-to-tdd/courseReady.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-echo "Preparing environment - this can take a few mins"
-
 EXISTS="1"
 while [ "$EXISTS" == "1" ]; do
     command -v python3 > /dev/null
     EXISTS=$?
-    echo "waiting"
+    echo "Preparing environment - this can take a while"
     sleep 1
 done
+
+echo "Environment ready!"
